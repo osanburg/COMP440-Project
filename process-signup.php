ea4e21865cf39094894b12d4cc49e7e066f8c123
JEspinoza501 <98982250+JEspinoza501@users.noreply.github.com>
2023-03-26 02:17:44 -0700

    added UI functionality
    https://youtu.be/5L9UhOnuos0  followed side by side this video to create the signup/login page, in the video, there is a database.php that i did not add yet, but will do next, as well as connect the database.

---
+++ process-signup.php
@@ -0,0 +1,58 @@
+<?php
+
+if (empty($_POST["name"])){
+    die("Name is required");
+}
+
+if (filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+    die("Valid email is required");
+}
+
+if (strlen($_POST["password"]) < 8){
+    die("Password must be at least 8 characters");
+}
+
+if (!preg_match("/[a-z]/i", $_POST["password"])){
+    die("Password must contain at least one letter");
+}
+
+if($_POST["password"] !== $_POST["password_confirmation"]){
+    die("Passwords must match");
+}
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+//olaf part//
+$mysqli = require __DIR__ . "/.php";
+
+$sql = "INSERT INTO user(username, firstName, lastName, email, password)
+        VALUES (?, ?, ?, ?, ?)";
+
+$stmt = $mysqli -> stmt_init();
+
+if (! $stmt -> prepare($sql)){
+    die ("SQL error: " . $mysqli -> error);
+}
+
+$stmt ->bind_param("sssss",
+                   $_POST["username"],
+                   $_POST["firstName"],
+                   $_POST["lastName"],
+                   $_POST["email"],
+                   $_POST["password"]);
+
+if ($stmt -> execute()){
+
+    header("Location: signup-success.html");
+    exit;
+
+} else {
+    
+    if ($mysqli->errno === 1062){
+        die("email already taken");
+    } else {
+    die($mysqli -> error . "  " . $mysqli->errno);
+    }
+}
+
+
